0cf3230307ff089caab4df0af4031b5a68f36894
btripoloni <bruno.tripoloni@gmail.com>
2025-12-19 15:00:44 -0300

    feat: initialize CLI with Cobra and define the root command

---
+++ internal/cli/root.go
@@ -0,0 +1,39 @@
+package cli
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "composer",
+	Short: "A declarative mod manager for Linux",
+	Long: `Composer is the core engine of the Mod Manager.
+It executes build plans (JSON DAGs) to create isolated, reproducible modpacks using Linux technologies like OverlayFS and Namespaces.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.composer.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
