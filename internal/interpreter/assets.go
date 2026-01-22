ddf049e27a542b3647ea7eec2554284df68a4b98
btripoloni <bruno.tripoloni@gmail.com>
2026-01-22 18:25:02 -0300

    refactor: Remove obsolete TypeScript assets and related files after migrating to JSR package

--- internal/interpreter/assets.go
+++
@@ -1,5 +0,0 @@
-package interpreter
-
-// This file previously contained embedded TypeScript assets.
-// The TypeScript library has been moved to a separate JSR package: jsr:@btripoloni/kintsugi
-// The ExtractAssets function is no longer needed as the library is now imported via JSR.
